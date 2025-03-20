16652c11550dc3352670812e55ff4b5b0bef4fc7
Gustavo Maia <ms.gustavo@outlook.com>
2025-03-20 19:09:08 -0300

    starting project

---
+++ src/main/java/com/myapp/itau_challenge/ItauUnibancoProgrammingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.itau_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItauUnibancoProgrammingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItauUnibancoProgrammingChallengeApplication.class, args);
+	}
+
+}
