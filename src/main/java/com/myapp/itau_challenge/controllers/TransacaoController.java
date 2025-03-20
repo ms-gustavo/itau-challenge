22da440dd6e7f8b8cf49410e15e926aa80f7db9f
Gustavo Maia <ms.gustavo@outlook.com>
2025-03-20 20:03:52 -0300

    creating transacaoController and routes

---
+++ src/main/java/com/myapp/itau_challenge/controllers/TransacaoController.java
@@ -0,0 +1,38 @@
+package com.myapp.itau_challenge.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.myapp.itau_challenge.dto.TransacaoDTO;
+import com.myapp.itau_challenge.model.Transacao;
+import com.myapp.itau_challenge.services.TransacaoService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/transacao")
+@RequiredArgsConstructor
+public class TransacaoController {
+
+	private final TransacaoService transacaoService;
+	
+	@PostMapping
+	public ResponseEntity<Void> criarTransacao(@Valid @RequestBody TransacaoDTO transacaoDTO){
+		Transacao transacao = new Transacao(transacaoDTO.valor(), transacaoDTO.dataHora());
+		Boolean sucesso = transacaoService.adicionarTransaction(transacao);
+		
+		return sucesso ? ResponseEntity.status(HttpStatus.CREATED).build() : ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+	}
+	
+	@DeleteMapping
+	public ResponseEntity<Void> deletarTransacoes(){
+		transacaoService.limparTransacoes();
+		return ResponseEntity.status(HttpStatus.OK).build();
+	}
+}
