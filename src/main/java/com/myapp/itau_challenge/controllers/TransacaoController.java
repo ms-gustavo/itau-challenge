244ad93c6b06764da5f47cc9135ae72e42301c0e
Gustavo Maia <ms.gustavo@outlook.com>
2025-03-21 15:42:12 -0300

    adding annotations in controllers

--- src/main/java/com/myapp/itau_challenge/controllers/TransacaoController.java
+++ src/main/java/com/myapp/itau_challenge/controllers/TransacaoController.java
@@ -14,17 +14,27 @@ import com.myapp.itau_challenge.dto.TransacaoDTO;
 import com.myapp.itau_challenge.model.Transacao;
 import com.myapp.itau_challenge.services.TransacaoService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/")
 @RequiredArgsConstructor
+@Tag(name = "Transações", description = "Gerenciamento de transações e estatísticas")
 public class TransacaoController {
 
 	private final TransacaoService transacaoService;
 
 	@PostMapping("/transacao")
+	@Operation(summary = "Criar uma nova transação", description = "Adiciona uma nova transação se for válida", responses = {
+			@ApiResponse(responseCode = "201", description = "Transação criada com sucesso"),
+			@ApiResponse(responseCode = "422", description = "Erro de validação", content = @Content(schema = @Schema(implementation = TransacaoDTO.class))),
+			@ApiResponse(responseCode = "400", description = "JSON inválido") })
 	public ResponseEntity<Void> criarTransacao(@Valid @RequestBody TransacaoDTO transacaoDTO) {
 		Transacao transacao = new Transacao(transacaoDTO.valor(), transacaoDTO.dataHora());
 		transacaoService.adicionarTransacao(transacao);
@@ -33,12 +43,17 @@ public class TransacaoController {
 	}
 
 	@DeleteMapping("/transacao")
+	@Operation(summary = "Deletar todas as transações", description = "Remove todas as transações da memória", responses = {
+			@ApiResponse(responseCode = "200", description = "Transações apagadas com sucesso") })
+
 	public ResponseEntity<Void> deletarTransacoes() {
 		transacaoService.limparTransacoes();
 		return ResponseEntity.status(HttpStatus.OK).build();
 	}
 
 	@GetMapping("/estatistica")
+	@Operation(summary = "Obter estatísticas", description = "Retorna as estatísticas das transações nos últimos 60 segundos", responses = {
+			@ApiResponse(responseCode = "200", description = "Estatísticas calculadas com sucesso", content = @Content(schema = @Schema(implementation = EstatisticaDTO.class))) })
 	public ResponseEntity<EstatisticaDTO> obterEstatisticas() {
 		EstatisticaDTO transacoes = transacaoService.calcularEstatisticas();
 		return ResponseEntity.status(HttpStatus.OK).body(transacoes);
